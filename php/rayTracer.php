9fbc197e2b286ff3b5c64ce38962d3e8c63e2069
Nathan Douglas <nd9600@gmail.com>
2019-03-03 18:03:00 +0000

    gamma correction

--- php/rayTracer.php
+++ php/rayTracer.php
@@ -119,6 +119,10 @@ function writeFile($file, int $scale = 1)
             }
             $col = $col->divideByConstant($numberOfSamples);
 
+            // it should be light grey, but image viewers assume that the image is “gamma corrected” (meaning the 0 to 1 values have some transform before being stored as a byte)
+            // so we have to correct for that: we can use “gamma 2” which means raising the color to the power 1/gamma, or 1/2 in the simple case
+            $col = new Vec3(sqrt($col->r()), sqrt($col->g()), sqrt($col->b()));
+
             $ir = intval(255.99 * $col[0]);
             $ig = intval(255.99 * $col[1]);
             $ib = intval(255.99 * $col[2]);
